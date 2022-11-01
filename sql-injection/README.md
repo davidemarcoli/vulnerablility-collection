7f475ec1682c36ed3d5294581a81d65fa313af6c
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-11-01 13:50:09 +0100

    Create Simple Nodejs server with db connection and login endpoint with a html form.

---
+++ sql-injection/README.md
@@ -0,0 +1,5 @@
+To start docker compose file run:
+`docker compose up`
+
+To run nodejs application past following command in the cli:
+`node hellonode.js`
--- sql-injection/hellonode.js
+++ sql-injection/hellonode.js
@@ -1,4 +1,5 @@
 //Load HTTP module
+const { appendFile } = require("fs");
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3001;
@@ -7,15 +8,17 @@ var mysql = require("mysql");
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      '<html><body><h1>SQL Injection</h1><form action="login" method="post"><input type="text" name="username" placeholder="username"><input type="password" name="password" placeholder="password"><input type="submit" value="Login"></form></body></html>'
+    );
+  }
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
-
-//listen for request on port 3001, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  //res.setHeader("Content-Type", "text/plain");
+  //res.end("Hello World\n");
+  return res.end();
 });
 
 var con = mysql.createConnection({
@@ -24,6 +27,34 @@ var con = mysql.createConnection({
   password: "password",
   database: "mydb",
   port: 3307,
+});
+
+server.get("/login", function (req, res) {
+  var username = req.query.username;
+  var password = req.query.password;
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "SELECT * FROM users WHERE username = '" + username + "'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      if (result.length > 0) {
+        if (result[0].password == password) {
+          res.send("Login Success");
+        } else {
+          res.send("Login Failed");
+        }
+      } else {
+        res.send("Login Failed");
+      }
+    });
+  });
+});
+
+//listen for request on port 3001, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 con.connect(function (err) {
